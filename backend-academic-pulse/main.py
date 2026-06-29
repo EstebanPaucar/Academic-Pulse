a45781dd04630d05402b91a3c6941cd86705dcd3
EstebanPaucar <tebanpaucar04@hotmail.com>
2026-06-29 15:35:57 -0500

    new main.py

--- backend-academic-pulse/main.py
+++ backend-academic-pulse/main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
+from src.core.shared.infrastructure.sqlalchemy_base import Base
+from src.core.config.database import engine
 from src.modules.auth.api.auth_router import auth_router
+
+# 🚨 EL FIX: Importamos el modelo explícitamente para que SQLAlchemy lo detecte
+from src.modules.auth.infra.usuario_model import UsuarioModel
+
+# Ahora SQLAlchemy sí sabe que 'UsuarioModel' existe y creará la tabla
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Academic Pulse API",
@@ -11,4 +19,4 @@ app.include_router(auth_router)
 
 @app.get("/")
 def root():
-    return {"message": "API Operativa 🚀"}
+    return {"message": "API Operativa 🚀 conectada a PostgreSQL"}
