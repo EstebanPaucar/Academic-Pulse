88446b38cb4f6c7b8472d6a78a08bf2c385c8175
EstebanPaucar <tebanpaucar04@hotmail.com>
2026-06-30 04:22:57 -0500

    upload frontend

--- frontend-campus-flow/config/theme.ts
+++ frontend-campus-flow/config/theme.ts
@@ -1,17 +1,16 @@
 export const theme = {
   colors: {
     primary: '#3B82F6',
-    secondary: '#10B981',   // Disponible
-    tertiary: '#F59E0B',    // Próximo
-    error: '#FF0000',       // Ocupado / Error
-    neutral: '#757780',     // Textos secundarios
-    background: '#F4F6F8',  // Fondo gris muy claro para que las tarjetas blancas resalten
+    secondary: '#10B981',
+    tertiary: '#F59E0B',
+    error: '#FF0000',
+    neutral: '#757780',
+    background: '#F4F6F8',
     white: '#FFFFFF',
     black: '#000000',
     border: '#E5E7EB',
   },
   typography: {
-    // Tamaños basados en tu Figma
     h1: 48,
     h2: 36,
     h3: 24,
