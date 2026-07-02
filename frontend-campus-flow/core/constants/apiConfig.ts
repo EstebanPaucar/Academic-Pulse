3ffa0e3b6335dcb54e2b436b865666c577c3c22f
EstebanPaucar <tebanpaucar04@hotmail.com>
2026-07-02 15:04:38 -0500

    Actually to Clerk

--- frontend-campus-flow/core/constants/apiConfig.ts
+++ frontend-campus-flow/core/constants/apiConfig.ts
@@ -1,2 +1,3 @@
 // Usa 10.0.2.2 para emuladores Android locales, o tu IP IPv4 si usas dispositivo físico.
+//export const API_URL = 'http://192.168.1.8:8000/api/v1';
 export const API_URL = 'http://10.0.2.2:8000/api/v1';
